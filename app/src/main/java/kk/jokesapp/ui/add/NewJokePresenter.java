81116211a31bb9da91f216b02016aa27122ab209
Kókai Kristóf <archetiz@sch.bme.hu>
2021-04-28 07:58:16 +0200

    Add database interaction to new joke addition aswell

--- app/src/main/java/kk/jokesapp/ui/add/NewJokePresenter.java
+++ app/src/main/java/kk/jokesapp/ui/add/NewJokePresenter.java
@@ -1,7 +1,15 @@
 package kk.jokesapp.ui.add;
 
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.concurrent.Executor;
+
 import javax.inject.Inject;
 
+import kk.jokesapp.annotation.DatabaseExecutor;
+import kk.jokesapp.event.JokeAddedEvent;
 import kk.jokesapp.interactor.CollectionsInteractor;
 import kk.jokesapp.model.Joke;
 import kk.jokesapp.ui.Presenter;
@@ -11,6 +19,10 @@ public class NewJokePresenter extends Presenter<NewJokeScreen> {
     @Inject
     CollectionsInteractor collectionsInteractor;
 
+    @DatabaseExecutor
+    @Inject
+    Executor databaseExecutor;
+
     @Inject
     public NewJokePresenter() {
     }
@@ -18,16 +30,30 @@ public class NewJokePresenter extends Presenter<NewJokeScreen> {
     @Override
     public void attachScreen(NewJokeScreen screen) {
         super.attachScreen(screen);
+        EventBus.getDefault().register(this);
     }
 
     @Override
     public void detachScreen() {
+        EventBus.getDefault().unregister(this);
         super.detachScreen();
     }
 
     public void saveNewJoke(String type, String setup, String punchline) {
-        Joke dummyJoke = new Joke(0, "dummy", "Dummy setup", "Dummy punchline");    //Temporarily
-        collectionsInteractor.addJoke(dummyJoke);
+
+        //TODO: Checks here...
+
+        databaseExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                Joke dummyJoke = new Joke(0, "dummy", "Dummy setup", "Dummy punchline");    //Temporarily
+                collectionsInteractor.addJoke(dummyJoke);
+            }
+        });
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onEventMainThread(final JokeAddedEvent event) {
         screen.showSaveResult(true, null);
     }
 }
