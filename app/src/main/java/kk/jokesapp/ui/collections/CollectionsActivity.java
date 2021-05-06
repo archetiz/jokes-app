61112e05258bd90d1e343f1170fc278b69a08cc3
Kókai Kristóf <archetiz@sch.bme.hu>
2021-05-06 16:07:36 +0200

    Add navigation

--- app/src/main/java/kk/jokesapp/ui/collections/CollectionsActivity.java
+++ app/src/main/java/kk/jokesapp/ui/collections/CollectionsActivity.java
@@ -1,9 +1,14 @@
 package kk.jokesapp.ui.collections;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.List;
 
@@ -12,6 +17,8 @@ import javax.inject.Inject;
 import dagger.hilt.android.AndroidEntryPoint;
 import kk.jokesapp.R;
 import kk.jokesapp.model.Joke;
+import kk.jokesapp.ui.add.NewJokeActivity;
+import kk.jokesapp.ui.joke.RandomJokeActivity;
 
 @AndroidEntryPoint
 public class CollectionsActivity extends AppCompatActivity implements CollectionsScreen {
@@ -19,15 +26,46 @@ public class CollectionsActivity extends AppCompatActivity implements Collection
     @Inject
     CollectionsPresenter collectionsPresenter;
 
+    private TabLayout tabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collections);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == 0) {
+                    Intent intent = new Intent(getBaseContext(), RandomJokeActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+
+        ImageButton bAddNewJoke = findViewById(R.id.bAddNewJoke);
+        bAddNewJoke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), NewJokeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        tabLayout.getTabAt(1).select();
         collectionsPresenter.attachScreen(this);
         collectionsPresenter.listCollections();
     }
--- app/src/main/java/kk/jokesapp/ui/joke/RandomJokeActivity.java
+++ app/src/main/java/kk/jokesapp/ui/joke/RandomJokeActivity.java
@@ -2,18 +2,21 @@ package kk.jokesapp.ui.joke;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
 
 import javax.inject.Inject;
 
 import dagger.hilt.android.AndroidEntryPoint;
 import kk.jokesapp.R;
 import kk.jokesapp.model.Joke;
+import kk.jokesapp.ui.collections.CollectionsActivity;
 
 @AndroidEntryPoint
 public class RandomJokeActivity extends AppCompatActivity implements RandomJokeScreen {
@@ -24,6 +27,8 @@ public class RandomJokeActivity extends AppCompatActivity implements RandomJokeS
     private TextView tvRandomJokeSetup;
     private TextView tvRandomJokePunchline;
     private View rootView;
+
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +47,31 @@ public class RandomJokeActivity extends AppCompatActivity implements RandomJokeS
                 randomJokePresenter.saveCurrentJoke();
             }
         });
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == 0) {
+                    Intent intent = new Intent(getBaseContext(), CollectionsActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        tabLayout.getTabAt(0).select();
         randomJokePresenter.attachScreen(this);
         randomJokePresenter.showRandomJoke();
     }
